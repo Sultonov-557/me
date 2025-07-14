1646314539bdc9d12c6f627966967a5c1d77913a
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-07-14 13:05:20 +0500

    update

--- nvim/.config/nvim/lua/plugins/cutless.lua
+++
@@ -1,9 +0,0 @@
--- Lua
-return {
-  "gbprod/cutlass.nvim",
-  opts = {
-    -- your configuration comes here
-    -- or don't set opts to use the default settings
-    -- refer to the configuration section below
-  },
-}
