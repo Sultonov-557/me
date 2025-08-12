02e449d1654801cc6b40726357f413bb8d7f845b
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-08-12 13:29:09 +0500

    update

---
+++ hypr/.config/hypr/custom/scripts/start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+LOGFILE=~/mpvpaper.log
+while [[ -z "$(hyprctl monitors | grep 'Monitor ')" ]]; do
+	echo "Waiting for monitors to become active..."
+	sleep 1
+done
+
+hyprshade on vibrancy
+mpvpaper "eDP-1" "/mnt/d/wallpapers/honda-nsx-na1.1920x1080.mp4" &>$LOGFILE &
+
+# Launch Kitty in workspace 1
+hyprctl dispatch workspace 1
+kitty &
+
+# Launch Zen-Browser in workspace 2
+sleep 1 # Add a short delay to prevent race conditions
+hyprctl dispatch workspace 2
+flatpak run app.zen_browser.zen &
+
+# Launch Yandex-Music in workspace 3
+sleep 1
+hyprctl dispatch workspace 3
+yandex-music &
+
+# Launch Telegram in workspace 4
+sleep 1
+hyprctl dispatch workspace 4
+telegram-desktop &
