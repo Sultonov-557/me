0efda73d42fc374b92f82c0cc2a2b56de7cd2930
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-08-23 16:07:58 +0500

    added auto wallpaper

--- hypr/.config/hypr/custom/scripts/start.sh
+++ hypr/.config/hypr/custom/scripts/start.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-LOGFILE=~/mpvpaper.log
 while [[ -z "$(hyprctl monitors | grep 'Monitor ')" ]]; do
 	echo "Waiting for monitors to become active..."
 	sleep 1
 done
 
 hyprshade on vibrancy
-
-WALLPAPER_DIR="/mnt/d/me/wallpaper/"
-DISPLAY="eDP-1"
-
-# Kill any existing mpvpaper instance (so the new one takes over)
-pkill -x mpvpaper
-
-# Pick a random file from the folder
-RANDOM_WALL=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
-
-# Run mpvpaper with it
-mpvpaper "$DISPLAY" -o "no-audio loop" "$RANDOM_WALL" &
 
 # Launch Kitty in workspace 1
 hyprctl dispatch workspace 1
