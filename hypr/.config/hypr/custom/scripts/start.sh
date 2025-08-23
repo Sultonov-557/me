ba0ad74889941fe29726c70de4cf7f86bca8209f
Sultonov-557 <sultonovzerifboy@gmail.com>
2025-08-23 16:01:06 +0500

    added some wallpapers

--- hypr/.config/hypr/custom/scripts/start.sh
+++ hypr/.config/hypr/custom/scripts/start.sh
@@ -7,7 +7,18 @@ while [[ -z "$(hyprctl monitors | grep 'Monitor ')" ]]; do
 done
 
 hyprshade on vibrancy
-mpvpaper "eDP-1" -o "no-audio loop" "/mnt/d/wallpapers/honda-nsx-na1.1920x1080.mp4"
+
+WALLPAPER_DIR="/mnt/d/me/wallpaper/"
+DISPLAY="eDP-1"
+
+# Kill any existing mpvpaper instance (so the new one takes over)
+pkill -x mpvpaper
+
+# Pick a random file from the folder
+RANDOM_WALL=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+
+# Run mpvpaper with it
+mpvpaper "$DISPLAY" -o "no-audio loop" "$RANDOM_WALL" &
 
 # Launch Kitty in workspace 1
 hyprctl dispatch workspace 1
